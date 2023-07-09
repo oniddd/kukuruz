d147902793bdfae2b86b456303ad9dbaec543237
oniddd <109388829+oniddd@users.noreply.github.com>
2023-07-09 18:30:26 +0200

    Create fenwick.cpp

---
+++ fenwick.cpp
@@ -0,0 +1,22 @@
+template <typename T>
+struct Fenwick {
+private:
+    vector<T> BIT;
+    T query_internal(T x) {
+        T sum = 0;
+        for(; x > 0; x -= x & -x)
+            sum += BIT[x];
+        return sum;
+    }
+public:
+    Fenwick(T n) : BIT(n+1, 0) {}
+
+    void update(T x, T delta) {
+        for(; x <= n; x += x & -x)
+            BIT[x] += delta;
+    }
+    // b > a
+    T query(T a, T b) {
+        return query_internal(b) - query_internal(a-1);
+    }
+};
