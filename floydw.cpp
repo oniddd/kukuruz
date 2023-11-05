2b17ff5a33f94535f831f44775d19620fc5dae68
oniddd <109388829+oniddd@users.noreply.github.com>
2023-11-05 15:54:41 +0100

    Update floydw.cpp

--- floydw.cpp
+++ floydw.cpp
@@ -10,26 +10,45 @@
 // Note: G[i][j] represents the shortest path from vertex i to vertex j.
 // If G[i][j] = inf then vertex j is unreachable from vertex i.
 
-#include <iostream>
+// Ne dozvoljava negativne cikluse
+
+#include <bits/stdc++.h>
+#define all(x) x.begin(), x.end()
+#define nl '\n'
+#define sp ' '
 
 using namespace std;
+using ll = long long;
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
 
-#define inf 1e9
+ll G[505][505];
 
-const int MAX = 10000;
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); 
+	#ifdef DBG
+	freopen("in.txt", "r", stdin); 
+	#endif
 
-int G[MAX][MAX], N;
+	int n,m,q;
+	cin>>n>>m>>q;
 
-int main()
-{
-    for (int i = 0; i < N; ++i)
-        for (int j = 0; j < N; ++j)
-            G[i][j] = (i == j? 0 : inf);
+	for(int i = 1; i <= n; i++)
+		for(int j = 1; j <= n; j++)
+			G[i][j] = i == j ? 0 : 1e18;
 
-    //input
+	for(int i = 0; i < m; i++) {
+		ll a,b,c;
+		cin>>a>>b>>c;
+		G[a][b] = c;
+	}
 
-    for (int k = 0; k < N; ++k)
-        for (int i = 0; i < N; ++i)
-            for (int j = 0; j < N; ++j)
-                G[i][j] = min(G[i][j], G[i][k] + G[k][j]);
+	for(int k = 1; k <= n; k++)
+		for(int i = 1; i <= n; i++)
+			for(int j = 1; j <= n; j++)
+				if(G[i][k] < 1e18 && G[j][k] < 1e18)
+					G[i][j] = min(G[i][j], G[i][k] + G[k][j]);
+
+	return 0;
 }
