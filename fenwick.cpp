bf15f08ae690b4a4cd3fa70e1397d6840ec79000
oniddd <109388829+oniddd@users.noreply.github.com>
2023-12-31 15:15:43 +0100

    Update fenwick.cpp

--- fenwick.cpp
+++ fenwick.cpp
@@ -11,6 +11,14 @@ private:
 public:
     Fenwick(T n) : BIT(n+1, 0) {}
 
+    void construct() {
+        for(int i = 1; i <= n; i++) {
+			int r = i + (i & -i);
+			if(r <= n)
+				BIT[r] += BIT[i];
+		}
+	}
+
     void update(T x, T delta) {
         for(; x <= n; x += x & -x)
             BIT[x] += delta;
