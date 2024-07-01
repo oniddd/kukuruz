186f30d6cc645d7c612a8c831abaa21b6108d65e
oniddd <109388829+oniddd@users.noreply.github.com>
2024-07-01 11:15:43 +0200

    Update kamp.cpp

--- kamp.cpp
+++ kamp.cpp
@@ -8,69 +8,55 @@ using ll = long long;
 using pii = pair<int,int>;
 using pll = pair<ll,ll>;
 
-int val[200005], byval[200005], depth[500005], mapping[200005];
+int val[200005], byval[200005], mapping[200005], operation[500005];
 pii edges[300005], interval[500005];
 vector<pii> queries;
 vector<int> G[500005];
-bool removed[200005];
-
+bool removed[300005], additional[500005];
+ 
 const int N = (1 << 18);
 struct SegTree {
 	ll T[2*N+1];
-	
 	void build() {
 		for(int i = N-1; i > 0; i--)
 			T[i] = max(T[i*2], T[i*2+1]);
 	}
-	
 	void update(int p, int v) {
 		p += N;
 		T[p] = v;
-	
 		for(; p > 0; p /= 2)
 			T[p/2] = max(T[p], T[p^1]);
 	}
-	
 	ll query(int l, int r) {
 		ll res = 0;
 		l += N;
 		r += N;
-	
 		while(l <= r) {
 			if(l & 1)
 				res = max(res, T[l++]);
 			if(!(r & 1))
 				res = max(res, T[r--]);
-			
 			l /= 2;
 			r /= 2;
 		}
 		return res;
 	}
 };
-
 struct UF
 {
     vector<int> r, p, node;
-    UF(int n) : r(n+1, 0), p(n+1, 0), node(n+1)
-    {
+    UF(int n) : r(n+1, 0), p(n+1, 0), node(n+1) {
         iota(p.begin(), p.end(), 0);
         iota(node.begin(), node.end(), 0);
     }
-    
-    int f(int x)
-    {
+    int f(int x) {
         return (p[x] == x ? x : p[x] = f(p[x]));
     }
-    
-    void u(int i, int j)
-    {
+    void u(int i, int j) {
         int x = f(i), y = f(j);
         if (x == y)
             return;
-
-        if (r[x] >= r[y])
-        {
+        if (r[x] >= r[y]) {
             p[y] = x;
             if (r[x] == r[y])
                 r[x]++;
@@ -78,16 +64,10 @@ struct UF
         else
             p[x] = y;
     }
-
-    bool s(int i, int j)
-    {
-        return f(i) == f(j);
-    }
 };
 
 int tick = 0;
-void dfs(int u, int p, int d) {
-	depth[u] = d;
+void dfs(int u, int p) {
 	if(G[u].empty()) {
 		interval[u] = {tick, tick};
 		mapping[u] = tick;
@@ -98,7 +78,7 @@ void dfs(int u, int p, int d) {
 	int r = -1e9;
 	for(int v : G[u])
 		if(v != p) {
-			dfs(v, u, d+1);
+			dfs(v, u);
 			l = min(l, interval[v].first);
 			r = max(r, interval[v].second);
 		}
@@ -114,7 +94,6 @@ int main() {
 
 	int n,m,q;
 	cin>>n>>m>>q;
-
 	int oq = q;
 
 	for(int i = 1; i <= n; i++) {
@@ -154,37 +133,22 @@ int main() {
 		int d = uf.node[uf.f(b)];
 		uf.u(a,b);
 		uf.node[uf.f(a)] = nodecnt;
+		operation[i] = nodecnt;
+
+		if(i >= oq)
+			additional[nodecnt] = 1;
 
 		G[nodecnt].push_back(c);
 		if(c != d)
 			G[nodecnt].push_back(d);
 	}
-	/*
-	for(int i = m; i > 0; i--) {
-		++nodecnt;
-		auto [a, b] = edges[i];
-		int c = uf.node[uf.f(a)];
-		int d = uf.node[uf.f(b)];
-		uf.u(a,b);
-		uf.node[uf.f(a)] = nodecnt;
 
-		G[nodecnt].push_back(c);
-		if(c != d)
-			G[nodecnt].push_back(d);
-	}
-	*/
-
-	unordered_set<int> snodes;
+	unordered_set<int> nodes;
 	for(int i = 1; i <= n; i++)
-		snodes.insert(uf.node[uf.f(i)]);
+		nodes.insert(uf.node[uf.f(i)]);
 
-	vector<int> nodes(all(snodes));
-
-	int maxdepth = 0;
-	for(int u : nodes) {
-		dfs(u, -1, 1);
-		maxdepth = max(maxdepth, depth[u]);
-	}
+	for(int u : nodes)
+		dfs(u, -1);
 
 	auto st = SegTree();	
 	for(int i = 1; i <= n; i++) {
@@ -192,7 +156,7 @@ int main() {
 		st.T[N+j] = val[i];
 	}
 	st.build();
-		
+
 	/*
 	for(int i = 1; i <= nodecnt; i++) {
 		cout<<i<<": ["<<interval[i].first<<sp<<interval[i].second<<"]"<<nl;
@@ -214,34 +178,39 @@ int main() {
 	
 	cout<<endl;
 	*/
-	for(auto [t, x] : queries) {
+
+	for(int i = 0; i < oq; i++) {
+		auto [t, x] = queries[i];
 		if(t == 1) {
 			x = mapping[x];
 			for(auto u : nodes) {
 				auto [l, r] = interval[u];
 				if(x >= l && x <= r) {
 					int ans = st.query(l, r);
-					st.update(mapping[byval[ans]], 0);
+					if(ans)
+						st.update(mapping[byval[ans]], 0);
 					cout<<ans<<nl;
 					break;
 				}
 			}
 		} else {
+			int node = operation[i];
+			nodes.erase(node);
+			for(int v : G[node]) {
+				nodes.insert(v);		
+			}
+			/*
 			vector<int> newnodes;
 			for(int u : nodes) {
-				if(depth[u] < maxdepth)
-					for(int v : G[u])
-						newnodes.push_back(v);
-			}
-			if(newnodes.empty()) {
-				for(int u : nodes) {
-					for(int v : G[u])
-						newnodes.push_back(v);
+				if(G[u].empty() || additional[u]) {
+					newnodes.push_back(u);
+					continue;
 				}
+				for(int v : G[u])
+					newnodes.push_back(v);
 			}
 			nodes = newnodes;
-			for(int u : nodes)
-				maxdepth = max(maxdepth, depth[u]);
+			*/
 		}
 	}
 	return 0;
